3a065040e2a66eeb646ab94a0e653dc3d294bba4
Tiana Lopez <tianadavin@gmail.com>
2023-09-16 22:40:47 -0700

    Adds new functions

--- main.js
+++ main.js
@@ -3,7 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Makes error message hidden
+const errorDisplay = document.querySelector('#modal').className = 'hidden';
 
+//identifying the heart buttons
+const likeButtons = document.querySelectorAll('.like-glyph');
+
+//when clicked, change the heart and send to server
+function likeHeart(event) {
+  heart = event.target
+  mimicServerCall("url")
+  .then(() => {
+    if (heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART;
+      heart.className = 'activated-heart'
+    }
+    else {
+      heart.innerText = EMPTY_HEART
+      heart.className = ''
+    }
+  })
+  .catch(() => {
+    setTimeout(function() {errorDisplay.className = 'visible'}, 3000)
+  })
+}
+
+//adding event listener function
+
+  for (let button of likeButtons) {
+    button.addEventListener('click', likeHeart)
+  }
 
 
 
